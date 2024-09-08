5eaec2279bee56cca046da9a0d853ab9c244dba5
martin9le <sangsiyuan@me.com>
2024-09-09 00:35:52 +0800

    2024/09/09 周一  0:35:48.91, from home

--- easy_utils/obj_utils/enumerable_utils.py
+++ easy_utils/obj_utils/enumerable_utils.py
@@ -17,6 +17,7 @@ import pickle
 import json
 from typing import Union, Self, Type
 from collections import namedtuple
+import decimal
 
 # 项目模块
 
@@ -25,6 +26,132 @@ import numpy
 
 
 # 代码块
+
+def frange(*args) -> list:
+    """
+    生成保证浮点数位数的range list
+    :param args: <first, end, step, closed_interval=False>
+    :return:
+    """
+    first = 0
+    end = 1
+    step = 0.1
+    closed_interval = False
+    l = len(args)
+
+    if l <= 0:
+        first = 0
+        end = 1
+        step = 0.1
+        closed_interval = False
+    elif l == 1:
+        first = 0
+        end = args[0]
+        step = 1
+        closed_interval = False
+    elif l == 2:
+        first = args[0]
+        end = args[1]
+        step = 1
+        closed_interval = False
+    elif l == 3:
+        first = args[0]
+        end = args[1]
+        step = args[2]
+        closed_interval = False
+    elif l >= 4:
+        first = args[0]
+        end = args[1]
+        step = args[2]
+        closed_interval = args[3]
+
+    range_list = []
+    decimal_first = decimal.Decimal(str(first))
+    decimal_step = decimal.Decimal(str(step))
+    decimal_end = decimal.Decimal(str(end))
+    decimal_ranger = decimal_first
+
+    if end >= first:
+        while True:
+            if decimal_ranger < decimal_end:
+                range_list.append(float(decimal_ranger))
+            elif decimal_ranger == decimal_end and closed_interval is True:
+                range_list.append(float(decimal_ranger))
+            else:
+                break
+            decimal_ranger += decimal_step
+    else:
+        while True:
+            if decimal_ranger > decimal_end:
+                range_list.append(float(decimal_ranger))
+            elif decimal_ranger == decimal_end and closed_interval is True:
+                range_list.append(float(decimal_ranger))
+            else:
+                break
+            decimal_ranger += decimal_step
+    return range_list
+
+
+def finterval(*args) -> list:
+    """
+    生成均匀间隔序列
+    :param args: <first, end, step, closed_interval=False>
+    :return:
+    """
+    l = len(args)
+    first = 0
+    end = 1
+    num = 10
+    closed_interval = False
+
+    if l <= 0:
+        first = 0
+        end = 1
+        num = 10
+        closed_interval = False
+    elif l == 1:
+        first = 0
+        end = args[0]
+        num = 10
+        closed_interval = False
+    elif l == 2:
+        first = args[0]
+        end = args[1]
+        num = 10
+        closed_interval = False
+    elif l == 3:
+        first = args[0]
+        end = args[1]
+        num = args[2]
+        closed_interval = False
+    elif l >= 4:
+        first = args[0]
+        end = args[1]
+        num = args[2]
+        closed_interval = args[3]
+
+    decimal_first = decimal.Decimal(str(first))
+    decimal_num = decimal.Decimal(str(num))
+    decimal_end = decimal.Decimal(str(end))
+
+    decimal_step = (decimal_end - decimal_first) / decimal_num
+    return frange(decimal_first, decimal_end, decimal_step, False) if closed_interval is False \
+        else numpy.linspace(first, end, num).tolist()
+
+
+def np_frange(*args) -> numpy.array:
+    """
+    numpy.ndarray格式的range列表
+    """
+    return numpy.array(frange(*args))
+
+
+def np_finterval(*args) -> numpy.array:
+    """
+    numpy.ndarray格式的interval列表
+    """
+    return numpy.array(finterval(*args))
+
 
 def flatten(seq: Union[list, tuple, numpy.ndarray]) -> list:
     """
